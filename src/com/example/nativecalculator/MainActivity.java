c1b6814165b03ad3ac51437fc630db00165ac365
ITLAB <ITLAB@IT205>
2017-01-31 15:16:10 +0530

    second commit/EX2

--- src/com/example/nativecalculator/MainActivity.java
+++ src/com/example/nativecalculator/MainActivity.java
@@ -2,34 +2,81 @@ package com.example.nativecalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+//import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+public class MainActivity extends Activity implements OnClickListener
+{
+	EditText Num1;
+	EditText Num2;
+	Button Add;
+	Button Sub;
+	Button Mul;
+	Button Div;
+	TextView Result;
 
-public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		Num1 = (EditText) findViewById(R.id.id1);
+		Num2 = (EditText) findViewById(R.id.id2);
+		Add = (Button) findViewById(R.id.Add);
+		Sub = (Button) findViewById(R.id.Sub);
+		Mul = (Button) findViewById(R.id.Mul);
+		Div = (Button) findViewById(R.id.Div);
+		Result = (TextView) findViewById(R.id.textView);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		Add.setOnClickListener(this);
+		Sub.setOnClickListener(this);
+		Mul.setOnClickListener(this);
+		Div.setOnClickListener(this);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public void onClick(View v)
+	{
+
+		float num1 = 0;
+		float num2 = 0;
+		float result = 0;
+		String oper = "";
+
+		if (TextUtils.isEmpty(Num1.getText().toString())
+				|| TextUtils.isEmpty(Num2.getText().toString()))
+			return;
+
+		num1 = Float.parseFloat(Num1.getText().toString());
+		num2 = Float.parseFloat(Num2.getText().toString());
+
+		switch (v.getId())
+		{
+		case R.id.Add:
+			oper = "+";
+			result = num1 + num2;
+			break;
+		case R.id.Sub:
+			oper = "-";
+			result = num1 - num2;
+			break;
+		case R.id.Mul:
+			oper = "*";
+			result = num1 * num2;
+			break;
+		case R.id.Div:
+			oper = "/";
+			result = num1 / num2;
+			break;
+		default:
+			break;
+		}
+		Result.setText(num1 + " " + oper + " " + num2 + " = " + result);
+	}
 }
